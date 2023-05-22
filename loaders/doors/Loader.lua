d089db8c42c65aa6c45eee81a0f750e6e40c7f38
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-05-21 18:16:57 -0700

    Update doors latestloader

--- loaders/doors/Loader.lua
+++ loaders/doors/Loader.lua
@@ -1,4 +1,4 @@
 -- Load the latest copies of Mika's script bundle.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Doors"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Loader.lua"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/plamen6789/UtilitiesHub/main/UtilitiesGUI'))()
