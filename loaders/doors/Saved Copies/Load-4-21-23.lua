f627d74576b07c84c378ab2a101af8c9a34ed405
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-06-03 12:03:58 -0700

    Update Load-4-21-23.lua

--- loaders/doors/Saved Copies/Load-4-21-23.lua
+++ loaders/doors/Saved Copies/Load-4-21-23.lua
@@ -1,5 +1,5 @@
 -- Load all the saved scripts for Doors
 update = "4-21-23"
-loadString(game:HttpGet("https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/saves/doors/" .. update .. "%20Update/Vynixius.lua"))()
-loadString(game:HttpGet("https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/saves/doors/" .. update .. "%20Update/Darkrai-X.lua"))()
-loadString(game:HttpGet("https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/saves/doors/" .. update .. "%20Update/PlamenUtil.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/saves/doors/" .. update .. "%20Update/Vynixius.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/saves/doors/" .. update .. "%20Update/Darkrai-X.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/saves/doors/" .. update .. "%20Update/PlamenUtil.lua"))()
