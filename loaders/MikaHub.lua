e02ee36e517c0125a26b83ae85cb90b0a2b32798
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-06-08 10:00:48 -0700

    Create MikaHub.lua

---
+++ loaders/MikaHub.lua
@@ -0,0 +1,74 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local DarkDexV5 = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/utilities/DarkDexV5.lua"
+local InfiniteYield = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/utilities/InfiniteYield.lua"
+local DoorsHub = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/loaders/doors/DoorsHub.lua"
+local SWS = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/utilities/simplewalkspeed.lua"
+
+local Window = OrionLib:MakeWindow({Name = "Mika Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "MIKAHUB"})
+local PlayerTab = Window:MakeTab({
+	Name = "Utilities",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local PlayerSection = PlayerTab:AddSection({
+	Name = "Explorers"
+})
+
+
+PlayerSection:AddButton({
+	Name = "Dark Dex V5",
+	Callback = function()
+		loadstring(game:HttpGet(DarkDexV5))()
+	end
+})
+PlayerSection:AddButton({
+	Name = "Infinite Yield",
+	Callback = function()
+		loadstring(game:HttpGet(InfiniteYield))()
+	end
+})
+
+local Utils = PlayerTab:AddSection({
+	Name = "All-Game Utils"
+})
+Utils:AddButton({
+	Name = "SimpleWalkspeed (Closes Hub)",
+	Callback = function()
+		loadstring(game:HttpGet(SWS))()
+	end
+})
+
+local GameTab = Window:MakeTab({
+	Name = "Game-Specific",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Game = GameTab:AddSection({
+	Name = "Games"
+})
+Game:AddButton({
+	Name = "Doors (Closes Hub)",
+	Callback = function()
+		loadstring(game:HttpGet(DoorsHub))()
+	end
+})
+
+local SettingsTab = Window:MakeTab({
+	Name = "Settings",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local SettingsSection = SettingsTab:AddSection({
+	Name = "Settings"
+})
+
+SettingsSection:AddButton({
+	Name = "Destroy UI",
+	Callback = function()
+        OrionLib:Destroy()
+  	end    
+})
+
+OrionLib:Init()
