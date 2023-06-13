0f84f58fdc8d46e8427e406de638c355cadeac45
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-06-13 14:16:59 -0700

    Update Loader.lua

--- loaders/doors/Loader.lua
+++ loaders/doors/Loader.lua
@@ -2,3 +2,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Doors"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Loader.lua"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/plamen6789/UtilitiesHub/main/UtilitiesGUI'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/scripts/DoorsMikamod.lua"))()
