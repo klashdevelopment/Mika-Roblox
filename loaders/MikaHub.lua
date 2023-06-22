0c217c490215a7530874dbcfb310ce6838b17ede
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-06-22 11:27:37 -0700

    Update MikaHub.lua

--- loaders/MikaHub.lua
+++ loaders/MikaHub.lua
@@ -1,8 +1,10 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
 local DarkDexV5 = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/utilities/DarkDexV5.lua"
 local InfiniteYield = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/utilities/InfiniteYield.lua"
 local DoorsHub = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/loaders/doors/DoorsHub.lua"
 local SWS = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/utilities/simplewalkspeed.lua"
+local SWS = "https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/utilities/StreamSniper.lua"
 
 local Window = OrionLib:MakeWindow({Name = "Mika Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "MIKAHUB"})
 local PlayerTab = Window:MakeTab({
@@ -30,12 +32,24 @@ PlayerSection:AddButton({
 })
 
 local Utils = PlayerTab:AddSection({
-	Name = "All-Game Utils"
+	Name = "Miscellaneous"
 })
 Utils:AddButton({
 	Name = "SimpleWalkspeed (Closes Hub)",
 	Callback = function()
 		loadstring(game:HttpGet(SWS))()
+	end
+})
+Utils:AddButton({
+	Name = "Stream Sniper",
+	Callback = function()
+		OrionLib:MakeNotification({
+			Name = "Stream Sniper",
+			Content = "This sniper was programmed 100% by SynapseX. All credit goes to them.",
+			Image = "rbxassetid://4483345998",
+			Time = 5
+		})
+		loadstring(game:HttpGet(SynapseXSniper))()
 	end
 })
 
