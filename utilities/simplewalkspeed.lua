97eb32aa41ac200fb4baafce87d47757e5eca08c
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-07-23 16:11:19 -0700

    Update simplewalkspeed.lua

--- utilities/simplewalkspeed.lua
+++ utilities/simplewalkspeed.lua
@@ -186,7 +186,7 @@ local function connect(signal, callback)
     return connection;
 end
 connect(game:GetService("UserInputService").InputBegan, function(input, processed)
-    if input.UserInputType.Name == "MouseButton1" and not processed and OrionLib.Flags["movement_teleporting_clicktp"].Value and game.PlaceId ~= CJRPGameId then
+    if game.PlaceId ~= CJRPGameId and input.UserInputType.Name == "MouseButton1" and not processed and OrionLib.Flags["movement_teleporting_clicktp"].Value then
         local character = game.Players.LocalPlayer.Character;
         local camPos = workspace.CurrentCamera.CFrame.Position;
 		local mouse = game.Players.LocalPlayer:GetMouse();
