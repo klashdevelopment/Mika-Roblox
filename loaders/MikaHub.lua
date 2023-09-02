945ecf4e939b802f3a1dcef278681318dcdefe48
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-09-02 12:48:38 -0700

    Update MikaHub.lua

--- loaders/MikaHub.lua
+++ loaders/MikaHub.lua
@@ -48,12 +48,6 @@ local PlayerSection = PlayerTab:AddSection({
 })
 
 PlayerSection:AddButton({
-	Name = "Dex V5 (cleaner)",
-	Callback = function()
-		loadstring(game:HttpGet(OldDarkDexV5))()
-	end
-})
-PlayerSection:AddButton({
 	Name = "Infinite Yield",
 	Callback = function()
 		loadstring(game:HttpGet(InfiniteYield))()
@@ -68,6 +62,12 @@ PlayerSection:AddButton({
 
 local Utils = PlayerTab:AddSection({
 	Name = "Miscellaneous"
+})
+Utils:AddButton({
+	Name = "Dex V5 (cleaner)",
+	Callback = function()
+		loadstring(game:HttpGet(OldDarkDexV5))()
+	end
 })
 Utils:AddButton({
 	Name = "orca",
@@ -103,6 +103,26 @@ Utils:AddButton({
 	Name = "SecureDex X3",
 	Callback = function()
 		loadstring(game:HttpGet(DarkDexV5))()
+	end
+})
+Utils:AddButton({
+	Name = "ChatBypass",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/synnyyy/synergy/additional/betterbypasser", true))({
+		    Method = 3, -- Method 1 is the main method. Method two is emojis. Method 3 is full transparency, no special symbols. Method 3 has been improved!
+		    Keybind = "F", -- Usually defaulted to F. You can change this keybind by replacing the string with a letter. Works for uppercase and lowercase.
+		    ShowMethodDictionary = true -- Shows you the full list of words that you can say with the method. Press FN + F9 to see this dictionary.
+		})
+	end
+})
+Utils:AddButton({
+	Name = "ChatBypass OLD",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/synnyyy/synergy/additional/betterbypasser", true))({
+		    Method = 1, -- Method 1 is the main method. Method two is emojis. Method 3 is full transparency, no special symbols. Method 3 has been improved!
+		    Keybind = "F", -- Usually defaulted to F. You can change this keybind by replacing the string with a letter. Works for uppercase and lowercase.
+		    ShowMethodDictionary = true -- Shows you the full list of words that you can say with the method. Press FN + F9 to see this dictionary.
+		})
 	end
 })
 
