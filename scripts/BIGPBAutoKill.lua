e372007b8d05a0ae5e3e8433fdc9d13697babba6
Gavin Fox <57292172+GavinGoGaming@users.noreply.github.com>
2023-09-02 11:34:39 -0700

    Update BIGPBAutoKill.lua

--- scripts/BIGPBAutoKill.lua
+++ scripts/BIGPBAutoKill.lua
@@ -18,6 +18,19 @@ btn.Size = UDim2.fromOffset(200, 30)
 local temp = Instance.new("UICorner")
 temp.Parent = btn
 
+
+local btnR = Instance.new("TextButton")
+btnR.Name = "MB"
+btnR.Parent = gui
+btnR.Position = UDim2.new(0.2, 0, 0.2, 35)
+btnR.TextColor3 = Color3.fromRGB(0, 0, 0)
+btnR.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+btnR.Text = "Nokick Autokill (R)"
+btnR.AnchorPoint = Vector2.new(0.5, 0.5)
+btnR.Size = UDim2.fromOffset(200, 30)
+local temp = Instance.new("UICorner")
+temp.Parent = btnR
+
 local savedTp = CFrame.new(0, 0, 0)
 local beenSaved = false
 
@@ -32,7 +45,95 @@ btn2.AnchorPoint = Vector2.new(0.5, 0.5)
 btn2.Size = UDim2.fromOffset(200, 30)
 local temp2 = Instance.new("UICorner")
 temp2.Parent = btn2
+function lookat(playerone, playertwo)
+	root_part_one = playerone.HumanoidRootPart
+	root_part_two = playertwo.HumanoidRootPart
+	root_part_one.CFrame = CFrame.new(root_part_one.Position, root_part_two.Position)
+end
 
+function tween()
+if cooldown then return end
+	local PrePoint = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+
+	cooldown = true
+	local t = Instance.new("TextLabel")
+	t.Parent = gui
+	t.Text = "3 - Mika"
+	t.AnchorPoint = Vector2.new(0.5, 0.5)
+	t.Position = UDim2.fromScale(0.5, 0.3)
+	t.TextSize = 20
+	t.ZIndex = 2000
+	wait(1)
+	t.Text = "2 - Mika"
+	wait(1)
+	t.Text = "1 - Mika"
+	wait(1)
+	btnR.Text = "RUNNING..."
+	t:Destroy()
+
+		for _,p in pairs(game.Players:GetPlayers()) do
+		local function set()
+			if game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.X - p.Character.HumanoidRootPart.CFrame.X > 200 then
+				return
+			end
+			if game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Z - p.Character.HumanoidRootPart.CFrame.Z > 200 then
+				return
+			end
+			game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(0.5),{CFrame = CFrame.new(
+				p.Character.HumanoidRootPart.CFrame.X,
+				p.Character.HumanoidRootPart.CFrame.Y + 3,
+				p.Character.HumanoidRootPart.CFrame.Z
+			)}):Play()
+			lookat(game.Players.LocalPlayer.Character, p.Character)
+		end
+			if game.Players.LocalPlayer.Team == nil then
+				if p ~= game.Players.LocalPlayer then
+					if not game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then break end
+					set()
+					wait(0.2)
+				end
+			else
+				if p ~= game.Players.LocalPlayer and p.Team ~= game.Players.LocalPlayer.Team then
+					if not game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then break end
+					set()
+					wait(0.2)
+					set()
+					wait(0.2)
+					set()
+					wait(0.2)
+					set()
+					wait(0.2)
+					set()
+					wait(0.2)
+					set()
+					wait(0.2)
+					set()
+					wait(0.1)
+				end
+			end
+		end
+		game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(0.5),{CFrame=PrePoint}):Play()
+	wait(1)
+	btnR.Text = "ON COOLDOWN (9)"
+	wait(1)
+	btnR.Text = "ON COOLDOWN (8)"
+	wait(1)
+	btnR.Text = "ON COOLDOWN (7)"
+	wait(1)
+	btnR.Text = "ON COOLDOWN (6)"
+	wait(1)
+	btnR.Text = "ON COOLDOWN (5)"
+	wait(1)
+	btnR.Text = "ON COOLDOWN (4)"
+	wait(1)
+	btnR.Text = "ON COOLDOWN (3)"
+	wait(1)
+	btnR.Text = "ON COOLDOWN (2)"
+	wait(1)
+	btnR.Text = "ON COOLDOWN (1)"
+	cooldown = false
+	btnR.Text = "Nokick Autokill (R)"
+end
 function run()
 if cooldown then return end
 	local PrePoint = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
@@ -109,12 +210,16 @@ if cooldown then return end
 	cooldown = false
 	btn.Text = "Autokill (F)"
 end
+btnR.MouseButton1Click:Connect(tween)
 btn.MouseButton1Click:Connect(run)
 
 game:GetService("UserInputService").InputBegan:Connect(function(input, gpe)
 	if gpe then return end
 	if input.KeyCode == Enum.KeyCode.F then
 		run()
+	end
+	if input.KeyCode == Enum.KeyCode.R then
+		tween()
 	end
 	if input.KeyCode == Enum.KeyCode.X then
 		if game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
