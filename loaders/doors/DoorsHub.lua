98967ca66763aaca8967f7ed060b7f6edb6eda0e
Gavin Fox <gavingogaming@gmail.com>
2024-03-25 16:27:31 -0700

    Update DoorsHub.lua

--- loaders/doors/DoorsHub.lua
+++ loaders/doors/DoorsHub.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/klashdevelopment/Mika-Roblox/main/libraries/Orion.lua')))()
 
 OrionLib:MakeNotification({
 	Name = "Mika",
